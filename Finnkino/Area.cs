bbd3d62127405f9d4077964a3fd161118ccb1ff7
Aleksi Olkkonen <aleksi.olkkonen93@gmail.com>
2015-12-01 13:20:07 +0200

    teatterin valinta toimii
    varmakaitseon

--- Finnkino/Area.cs
+++ Finnkino/Area.cs
@@ -3,18 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace Finnkino
 {
+    [XmlType("TheatreArea")]
     public class Area
     {
+        [XmlElement("ID")]
         public int AreaId { get; set; }
+        [XmlElement("Name")]
         public string Theatre { get; set; }
 
-        public Area(int _areaId, string _theatre)
-        {
-            this.AreaId = _areaId;
-            this.Theatre = _theatre;
-        }
     }
 }
---
+++ Finnkino/TheatreAreas.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Finnkino
+{
+    [XmlRoot("TheatreAreas")]
+    public class TheatreAreas
+    {
+   
+           
+        [XmlElement("TheatreArea")]
+        public List<Area> TheatreArea { get; set; }
+        }
+    
+}
